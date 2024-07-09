51b1ad4580a66a597f11f5e5942ed8bcf14ea4c8
Aman <amannigam18082001@gmail.com>
2024-07-09 13:38:16 +0530

    register and login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ListPage from "./routes/listPage/ListPage"
 import Layout from "./routes/layout/Layout"
 import SinglePage from "./routes/singlePage/SinglePage"
 import Profile from "./routes/profile/Profile"
+import Register from "./routes/register/Register"
+import Login from "./routes/login/Login"
 
 function App() {
   const router = createBrowserRouter([
@@ -26,6 +28,14 @@ function App() {
         {
           path:"/profile",
           element:<Profile/>
+        },
+        {
+          path:"/login",
+          element:<Login/>
+        },
+        {
+          path:"/register",
+          element:<Register/>
         }
       ]
     },
